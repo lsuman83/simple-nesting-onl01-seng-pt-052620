f0d84c9b6c46e26ffa5bd33e9b998469f8f0e4a2
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-06-02 01:28:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,7 +105,7 @@ def changing_alan
     programmer_hash.each do |name, data|
       if name == :alan_kay      
         data.each do |attribute, value|
-          value = alans_new_info
+          value.replace(alans_new_info)
         end
           
       end
